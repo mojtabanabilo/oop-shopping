1a08bfaa8a13b08dcfa208291af577005186aa3d
mojtaba nabilu <mojjtaba00@gmail.com>
2024-05-30 15:28:46 +0330

    completed cart and project

--- models/Cart.js
+++ models/Cart.js
@@ -4,10 +4,16 @@ export default class Cart {
     this.price = price;
     this.product = [];
     this.duplicateArray = [];
+    this.element.addEventListener("click", this);
   }
 
   showProducts() {
     this.duplicateArray = [...new Set(this.product)];
+    if (this.product.length === 0) this.element.innerHTML = `<h5>Empty</h5>`;
+    this.price.innerText = `$ ${this.product.reduce(
+      (acc, cur) => acc + cur.price,
+      0
+    )}`;
     if (this.duplicateArray.length > 0) this.element.innerHTML = "";
     this.duplicateArray.forEach((item) => {
       const qty = this.product.filter((p) => p.id === item.id).length;
@@ -34,12 +40,15 @@ export default class Cart {
   }
 
   productInfo(data) {
-    const { name, price } = data;
+    const { name, id } = data;
+    const sunProductPrice = this.product
+      .filter((item) => item.id === id)
+      .reduce((acc, cur) => acc + cur.price, 0);
 
     const infoJSX = `
       <div id="cart-info">
         <h4>${name}</h4>
-        <p>$ ${price}</p>
+        <p>$ ${sunProductPrice}</p>
       </div>
     `;
 
@@ -61,5 +70,30 @@ export default class Cart {
     `;
 
     return controlsJSX;
+  }
+
+  handleEvent() {
+    const id = event.target.dataset.id;
+    const type = event.target.innerText;
+    if (event.target.tagName === "BUTTON") {
+      switch (type) {
+        case "+":
+          const increase = this.product.find((item) => item.id === +id);
+          this.product.push(increase);
+          this.showProducts();
+          break;
+        case "-":
+          this.product.filter((item) => item.id === +id);
+          const decrease = this.product.findIndex((item) => item.id === +id);
+          this.product.splice(decrease, 1);
+          this.showProducts();
+          break;
+        case "Remove":
+          const remove = this.product.filter((item) => item.id !== +id);
+          this.product = remove;
+          this.showProducts();
+          break;
+      }
+    }
   }
 }
